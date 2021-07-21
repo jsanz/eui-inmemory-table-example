7fa4485b3c7da7cba49f872c583dd0991bcf2c4a
Jorge Sanz <jorge.sanz@elastic.co>
2021-07-21 17:10:28 +0200

    eui minimal memory table example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import '@elastic/eui/dist/eui_theme_amsterdam_light.css';
+import {
+  EuiPage,
+  EuiPageContent,
+  EuiPageBody,
+  EuiPageContentBody,
+  EuiInMemoryTable,
+} from '@elastic/eui';
+import { LoremIpsum } from "lorem-ipsum";
+
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: { max: 8, min: 4 },
+  wordsPerSentence: { max: 16, min: 4 }
+});
 
 function App() {
+
+  const rows = Array.from(Array(30).keys()).map(id => {
+    return {
+      id: id + 1,
+      desc: lorem.generateSentences(1)
+    }
+  });
+
+  const columns = [
+    {
+      field: 'id',
+      name: 'Identifier',
+      sortable: true,
+      width: '100px'
+    },
+    {
+      field: 'desc',
+      name: 'Description',
+      truncateText: true,
+    }
+  ];
+
+  const pagination = {
+    pageSize: 7,
+    pageSizeOptions: [7, 14, 28]
+  };
+
+  const sorting = {
+    sort: {
+      field: 'id',
+      direction: 'desc'
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <EuiPage>
+        <EuiPageBody>
+          <EuiPageContent>
+            <EuiPageContentBody>
+                <EuiInMemoryTable
+                  items={rows}
+                  columns={columns}
+                  pagination={pagination}
+                  sorting={sorting}
+                />
+            </EuiPageContentBody>
+          </EuiPageContent>
+        </EuiPageBody>
+      </EuiPage>
     </div>
   );
 }
